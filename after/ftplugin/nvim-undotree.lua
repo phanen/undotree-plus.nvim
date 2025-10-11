e5833c6173739b09da47348bd92130cbf8fdaa44
phanium <91544758+phanen@users.noreply.github.com>
2025-10-12 01:09:07 +0800

    fix: update diff win when undo in buffer

--- after/ftplugin/nvim-undotree.lua
+++ after/ftplugin/nvim-undotree.lua
@@ -1,3 +1,4 @@
+local api = vim.api
 local u = {
   undo = require('undotree-plus'),
   --- @param x any
@@ -11,14 +12,23 @@ local u = {
 
 local ut_buf = api.nvim_get_current_buf()
 
+local on_text_changed
+local render_diff
+render_diff = vim.schedule_wrap(function()
+  local buf = u.undo.buf_from_title(api.nvim_buf_get_name(ut_buf))
+  on_text_changed = on_text_changed
+    or api.nvim_create_autocmd('TextChanged', {
+      buffer = buf,
+      callback = render_diff,
+    })
+  local ut_win = assert(vim.b[buf].nvim_undotree)
+  local line = api.nvim_win_call(ut_win, api.nvim_get_current_line)
+  u.undo.render_diff(buf, u.asinteger(line:match('%d+')))
+end)
+
 api.nvim_create_autocmd('CursorMoved', {
   buffer = ut_buf,
-  callback = vim.schedule_wrap(function()
-    local buf = u.undo.buf_from_title(api.nvim_buf_get_name(ut_buf))
-    local ut_win = assert(vim.b[buf].nvim_undotree)
-    local line = api.nvim_win_call(ut_win, api.nvim_get_current_line)
-    u.undo.render_diff(buf, u.asinteger(line:match('%d+')))
-  end),
+  callback = render_diff,
 })
 
 api.nvim_create_autocmd('WinClosed', {
@@ -27,11 +37,10 @@ api.nvim_create_autocmd('WinClosed', {
 })
 
 local group = api.nvim_create_augroup('my.nvim.undotree', {})
-
 api.nvim_create_autocmd('BufEnter', {
   group = group,
   callback = vim.schedule_wrap(function(ev)
-    if not api.nvim_buf_is_valid(ut_buf) then return end
+    if not api.nvim_buf_is_valid(ut_buf) then return true end
     local buf = u.undo.buf_from_title(api.nvim_buf_get_name(ut_buf))
     if
       not api.nvim_buf_is_valid(ev.buf)
@@ -43,5 +52,7 @@ api.nvim_create_autocmd('BufEnter', {
     end
     u.undo.open({ buf = ut_buf, win = vim.b[buf].nvim_undotree })
     vim.b[buf].nvim_undotree = nil
+    pcall(api.nvim_del_autocmd, on_text_changed)
+    on_text_changed = nil
   end),
 })
